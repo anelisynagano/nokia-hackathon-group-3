6f368381fcb5333707c7ae50f44d343ba0cec18d
WildCodeSchool <anelisynagano@gmail.com>
2020-09-14 14:10:47 +0100

    Updates

---
+++ precache-manifest.704e2534eb89aaa451847b5334062583.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "584d4348f77b06240616a5edc49205c7",
+    "url": "/nokia-hackathon-group-3/index.html"
+  },
+  {
+    "revision": "822076da12422576ca8a",
+    "url": "/nokia-hackathon-group-3/static/css/main.d91e3964.chunk.css"
+  },
+  {
+    "revision": "d125099c8ce9b8e8a187",
+    "url": "/nokia-hackathon-group-3/static/js/2.f75b9e4c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/nokia-hackathon-group-3/static/js/2.f75b9e4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "822076da12422576ca8a",
+    "url": "/nokia-hackathon-group-3/static/js/main.febbbe29.chunk.js"
+  },
+  {
+    "revision": "2f4130549c5debec527a",
+    "url": "/nokia-hackathon-group-3/static/js/runtime-main.3a612f80.js"
+  },
+  {
+    "revision": "8ea042293b1c5612879c1c14c235be5e",
+    "url": "/nokia-hackathon-group-3/static/media/4g.8ea04229.svg"
+  },
+  {
+    "revision": "0d50c294272002c4b14d61c6e0ce3851",
+    "url": "/nokia-hackathon-group-3/static/media/Flashlight-512.0d50c294.png"
+  },
+  {
+    "revision": "ce77b4a82f378a5e58fbf01c98e2f59e",
+    "url": "/nokia-hackathon-group-3/static/media/NokiaPureHWeb_Bd.ce77b4a8.otf"
+  },
+  {
+    "revision": "9b44b8c1c041d91b6b056df9dcf41c32",
+    "url": "/nokia-hackathon-group-3/static/media/NokiaPureHWeb_Lt.9b44b8c1.otf"
+  },
+  {
+    "revision": "bfe70034ae318769c6a75023b3cc1e80",
+    "url": "/nokia-hackathon-group-3/static/media/NokiaPureHWeb_Rg.bfe70034.otf"
+  },
+  {
+    "revision": "d3eadc8d2baa41ab6ffebac26a3cf6b3",
+    "url": "/nokia-hackathon-group-3/static/media/NokiaPureTWeb_Bd.d3eadc8d.ttf"
+  },
+  {
+    "revision": "abb1b26db4e48c3b62940e45b06d87f9",
+    "url": "/nokia-hackathon-group-3/static/media/NokiaPureTWeb_Lt.abb1b26d.ttf"
+  },
+  {
+    "revision": "536160e84aba3fb83504066dd068bed3",
+    "url": "/nokia-hackathon-group-3/static/media/NokiaPureTWeb_Rg.536160e8.ttf"
+  },
+  {
+    "revision": "0217201b9b2994510b184e5679f858ad",
+    "url": "/nokia-hackathon-group-3/static/media/clean-battery.0217201b.svg"
+  }
+]);
